4cc66d6e29199a2e5bbbc19677d033bae27395fc
Meridith Lawn <meridithlawn2@gmail.com>
2023-05-24 21:33:44 -0600

    all tests passing

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,26 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves_list):
+    i = 1
+    for name in dwarves_list:
+        print(f'{i}. {name}')
+        i += 1
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    calls_list = [f'{call.title()}!' for call in planeteer_calls]
+    return calls_list
+    # summon_captain_planet(["cat", "dog", "bear"])
+    
+        
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(short_words):
+    for word in short_words:
+        if len(word) > 4:
+            return True
+    
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(foods):
+    cheeses = ["camembert", "gouda", "cheddar"]
+    for food in foods:
+        if food in cheeses:
+            return food
+    return None
